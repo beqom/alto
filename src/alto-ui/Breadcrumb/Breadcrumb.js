cae46e50fb1192dc82e798ce3b39c2b7f5545d48
Renaud Tertrais <renaud.tertrais@gmail.com>
2017-12-15 15:18:07 +0100

    Create breadcrumb component

---
+++ src/alto-ui/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+import ChevronLeft from '../Icons/ChevronLeft';
+import ChevronRight from '../Icons/ChevronRight';
+import './Breadcrumb.scss';
+
+const renderItems = items => {
+  if(items.length === 1) {
+    const { title, url } = items[0];
+    return (
+      <li className="breadcrumb__item">
+        <ChevronLeft className="breadcrumb__chevron" />
+        <a className="breadcrumb__link" href={url}>
+          Back to {title}
+        </a>
+      </li>
+    );
+  }
+
+  return items.map(({ title, url }, i) => (
+    <li className="breadcrumb__item" key={url}>
+      {i !== 0 && (
+        <ChevronRight className="breadcrumb__chevron" />
+      )}
+      <a className="breadcrumb__link" href={url}>{title}</a>
+    </li>
+  ));
+}
+
+const Breadcrumb = ({ items, className }) => {
+  if (!items || !items.length) return null;
+
+  return (
+    <ul className={classnames('breadcrumb', className)}>
+      {renderItems(items)}
+    </ul>
+  );
+};
+
+
+Breadcrumb.displayName = 'Breadcrumb';
+
+Breadcrumb.defaultProps = {
+};
+
+Breadcrumb.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  })),
+  className: PropTypes.string,
+};
+
+export default Breadcrumb;
---
+++ src/alto-ui/Breadcrumb/story.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { number} from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+import centered from '@storybook/addon-centered';
+
+import Breadcrumb from './Breadcrumb';
+import README from './README.md';
+
+const items = [
+  { title: 'First page', url: '#1' },
+  { title: 'List', url: '#2' },
+  { title: 'List details', url: '#3' },
+  { title: 'More details', url: '#4' },
+  { title: '???', url: '#5' },
+  { title: 'Nothing to see here', url: '#6' },
+];
+
+storiesOf('Breadcrumb', module)
+  .addDecorator(withReadme(README))
+  .addDecorator(centered)
+  .addWithJSX('playground', () => (
+    <Breadcrumb
+      items={items.slice(
+        0,
+        number('items', 3, { range: true, step: 1, min: 0, max: items.length })
+      )}
+    />
+  ));
