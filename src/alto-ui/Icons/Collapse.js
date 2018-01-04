dc5032b6e7e9aef1f0cf1fd971c0f2a771aed938
Andy McFee <andy.mcfee@gmail.com>
2018-01-04 16:01:28 +0100

    Add collapse and expand icons

---
+++ src/alto-ui/Icons/Collapse.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from './Icon';
+
+const Collapse = props => (
+  <Icon {...props}>
+    {ownProps => (
+      <g>
+        <path {...ownProps} d="M32,15H22.41l9.25-9.25a1,1,0,0,0-1.41-1.41L21,13.59V4a1,1,0,0,0-2,0V17H32a1,1,0,0,0,0-2Z"/>
+        <path {...ownProps} d="M4,19a1,1,0,0,0,0,2h9.59L4.33,30.25a1,1,0,1,0,1.41,1.41L15,22.41V32a1,1,0,0,0,2,0V19Z"/>
+      </g>
+    )}
+  </Icon>
+);
+
+Collapse.displayName = 'Collapse';
+
+export default Collapse;
---
+++ src/alto-ui/Icons/Expand.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from './Icon';
+
+const Expand = props => (
+  <Icon {...props}>
+    {ownProps => (
+      <g>
+        <path {...ownProps} d="M19,4a1,1,0,0,0,0,2h9.59l-9.25,9.25a1,1,0,1,0,1.41,1.41L30,7.41V17a1,1,0,0,0,2,0V4Z"/>
+        <path {...ownProps} d="M4,19a1,1,0,0,1,2,0v9.59l9.25-9.25a1,1,0,1,1,1.41,1.41L7.41,30H17a1,1,0,0,1,0,2H4Z"/>
+      </g>
+    )}
+  </Icon>
+);
+
+Expand.displayName = 'Expand';
+
+export default Expand;
