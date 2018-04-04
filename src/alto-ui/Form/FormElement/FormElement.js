ca5a8903198fdab6e5417c38d807b117ce145de4
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-04-04 17:40:43 +0200

    Add hideLabel option to FormElement

--- src/alto-ui/Form/FormElement/FormElement.js
+++ src/alto-ui/Form/FormElement/FormElement.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import { bemClass } from '../../helpers/bem';
 import Label from '../Label';
@@ -8,13 +9,16 @@ import './FormElement.scss';
 const FormElement = props => {
   const { error, disabled, success, id, children, readOnly } = props;
   return (
-    <div className="form-element">
-      <Label htmlFor={id} id={`${id}__label`} readOnly={readOnly}>
+    <div className={classnames('form-element', props.className)}>
+      <Label hidden={props.hideLabel} htmlFor={id} id={`${id}__label`} readOnly={readOnly}>
         {props.label}
       </Label>
       {children}
       {props.helpText && (
-        <div className={bemClass('form-element__help-text', { error, disabled, success })} id={`${id}__help-text`}>
+        <div
+          className={bemClass('form-element__help-text', { error, disabled, success })}
+          id={`${id}__help-text`}
+        >
           {props.helpText}
         </div>
       )}
@@ -26,10 +30,12 @@ FormElement.propTypes = {
   helpText: PropTypes.any,
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  className: PropTypes.string,
   error: PropTypes.bool,
   disabled: PropTypes.bool,
   success: PropTypes.bool,
   readOnly: PropTypes.bool,
+  hideLabel: PropTypes.bool,
   children: PropTypes.element.isRequired,
 };
 
