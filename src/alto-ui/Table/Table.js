c32418c1c0f4baec28ef3de76958047fa186dc79
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-04-10 17:34:27 +0200

    add single sorting + width in Table

--- src/alto-ui/Table/Table.js
+++ src/alto-ui/Table/Table.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 
 import CheckIcon from '../Icons/Check';
+import ChevronUpIcon from '../Icons/ChevronUp';
+import ChevronDownIcon from '../Icons/ChevronDown';
 import Avatar from '../Avatar';
 import { bemClass } from '../helpers/bem';
 
@@ -29,15 +31,74 @@ const renderCell = (row, props) => col => {
   const type = col.type || typeof value;
   const renderer = RENDERERS[type] || IDENTITY;
   const formatter = FORMATTERS[type] || IDENTITY;
+  const style = col.width || col.width === 0 ? { width: col.width, maxWidth: col.width } : {};
+
   return (
     <td
       key={key}
       className={bemClass('Table__cell', {
         [type]: true,
       })}
+      style={style}
     >
-      {renderer(formatter(value, col, props), col, props)}
+      <div className="Table__cell-content" style={style}>
+        {renderer(formatter(value, col, props), col, props)}
+      </div>
     </td>
+  );
+};
+
+const renderHeaderCell = p => col => {
+  const style = col.width || col.width === 0 ? { width: col.width, maxWidth: col.width } : {};
+  if (p.onSort) {
+    const sorted = col.key === p.columnSorted || [1, -1].includes(col.sortDirection);
+    return (
+      <th
+        key={col.key}
+        className={bemClass('Table__cell', {
+          header: true,
+          sortable: true,
+          sorted,
+        })}
+        style={style}
+      >
+        <button
+          className="Table__cell-button Table__cell-content Table__cell-content--header"
+          onClick={() => p.onSort(col)}
+          style={style}
+        >
+          <span className="Table__header-cell-title">{col.title}</span>
+          <div className="Table__header-cell-sortable-icons">
+            <div
+              className={bemClass('Table__header-cell-sortable-icon', {
+                active: (sorted && p.sortDirection === 1) || col.sortDirection === 1,
+              })}
+            >
+              <ChevronUpIcon />
+            </div>
+            <div
+              className={bemClass('Table__header-cell-sortable-icon', {
+                active: (sorted && p.sortDirection === -1) || col.sortDirection === -1,
+              })}
+            >
+              <ChevronDownIcon />
+            </div>
+          </div>
+        </button>
+      </th>
+    );
+  }
+
+  return (
+    <th
+      key={col.key}
+      style={style}
+      className={bemClass('Table__cell', {
+        header: true,
+      })}
+    >
+      <div className="Table__cell-content Table__cell-content--header">{col.title}</div>
+    </th>
   );
 };
 
@@ -48,13 +109,7 @@ const Table = props => {
     <div className={bemClass('Table', { wide, compact }, className)}>
       <table className="Table__table">
         <thead>
-          <tr>
-            {columns.map(col => (
-              <th key={col.key} className="Table__cell Table__cell--header">
-                {col.title}
-              </th>
-            ))}
-          </tr>
+          <tr>{columns.map(renderHeaderCell(props))}</tr>
         </thead>
         <tbody>
           {rows.map(row => <tr key={row[rowId]}>{columns.map(renderCell(row, props))}</tr>)}
@@ -68,6 +123,7 @@ Table.displayName = 'Table';
 
 Table.defaultProps = {
   rowId: 'id',
+  columnsSorted: [],
 };
 
 Table.propTypes = {
--- src/alto-ui/Table/story.js
+++ src/alto-ui/Table/story.js
@@ -5,51 +5,84 @@ import { boolean } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import centered from '@storybook/addon-centered';
 
+import StateProvider from '../StateProvider';
+
 import Table from './Table';
 import README from './README.md';
+
+const rows = [
+  {
+    id: 1,
+    name: 'Bart Simpson',
+    age: 12,
+    birth_date: '2000-05-17',
+    speak: true,
+    picture:
+      'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Bart-Simpson-01-icon.png',
+  },
+  {
+    id: 2,
+    name: 'Lisa Simpson',
+    age: 10,
+    birth_date: '2002-01-06',
+    speak: true,
+    picture: 'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Lisa-Simpson-icon.png',
+  },
+  {
+    id: 3,
+    name: 'Maggie Simpson',
+    age: 3,
+    birth_date: '2009-09-27',
+    speak: false,
+    picture: 'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Maggie-Simpson-icon.png',
+  },
+];
+
+const sort = (arr, col, direction) => {
+  if (!col) return arr;
+  return arr.slice().sort((a, b) => {
+    if (a[col] < b[col]) return -direction;
+    if (a[col] > b[col]) return direction;
+    return 0;
+  });
+};
 
 storiesOf('Table', module)
   .addDecorator(centered)
   .addDecorator(withReadme(README))
   .addWithJSX('playground', () => (
-    <Table
-      wide={boolean('wide', false)}
-      compact={boolean('compact', false)}
-      columns={[
-        { key: 'picture', title: 'Picture', type: 'image' },
-        { key: 'name', title: 'Name' },
-        { key: 'speak', title: 'Speak ?' },
-        { key: 'birth_date', title: 'Birth Date', type: 'date' },
-        { key: 'age', title: 'Age' },
-      ]}
-      rows={[
-        {
-          id: 1,
-          name: 'Bart Simpson',
-          age: 12,
-          birth_date: '2000-05-17',
-          speak: true,
-          picture:
-            'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Bart-Simpson-01-icon.png',
-        },
-        {
-          id: 2,
-          name: 'Lisa Simpson',
-          age: 10,
-          birth_date: '2002-01-06',
-          speak: true,
-          picture:
-            'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Lisa-Simpson-icon.png',
-        },
-        {
-          id: 3,
-          name: 'Maggie Simpson',
-          age: 3,
-          birth_date: '2009-09-27',
-          speak: false,
-          picture:
-            'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Maggie-Simpson-icon.png',
-        },
-      ]}
-    />
+    <StateProvider
+      state={{
+        columnSorted: 'age',
+        sortDirection: 'DESC',
+      }}
+    >
+      {(state, setState) => (
+        <Table
+          wide={boolean('wide', false)}
+          compact={boolean('compact', false)}
+          onSort={col => {
+            if (col.key === state.columnSorted) {
+              if (state.sortDirection === -1) {
+                setState({ columnSorted: null });
+              } else {
+                setState({ sortDirection: -1 });
+              }
+            } else {
+              setState({ columnSorted: col.key, sortDirection: 1 });
+            }
+          }}
+          columnSorted={state.columnSorted}
+          sortDirection={state.sortDirection}
+          columns={[
+            { key: 'picture', title: 'Picture', type: 'image' },
+            { key: 'name', title: 'Name' },
+            { key: 'speak', title: 'Speak ?' },
+            { key: 'birth_date', title: 'Birth Date', type: 'date' },
+            { key: 'age', title: 'Age' },
+          ]}
+          rows={sort(rows, state.columnSorted, state.sortDirection)}
+        />
+      )}
+    </StateProvider>
   ));
