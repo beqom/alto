9c9ad51bc9d3daee976cd3d52924b897f5fb5fc5
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-04-17 15:06:25 +0200

    Refactor Select (Sass...)

--- src/alto-ui/Form/Select/Select.js
+++ src/alto-ui/Form/Select/Select.js
@@ -1,70 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
 import isArray from 'lodash.isarray';
+import { bemClass } from '../../helpers/bem';
 
-import { getColor, getTheme, fontSize } from '../../helpers/theme';
 import FormElement from '../FormElement';
 
-const modifier = (...ms) => val => props =>
-  ms.reduce((acc, m) => acc && props[m], true) ? val : '';
-
-const SelectTag = styled.select`
-  ${fontSize('medium')};
-  outline: 0;
-  background: white;
-  color: ${getColor('text')};
-  border-radius: ${getTheme('borderRadius')};
-  border: 1px solid ${getColor('coolGrey.20')};
-  font-weight: 400;
-  height: 2.375em;
-  display: block;
-  width: 100%;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  transition: box-shadow ${getTheme('transition')}, border-color ${getTheme('transition')};
-
-  :hover {
-    box-shadow: 0 2px 1px rgba(0, 0, 0, 0.1);
-  }
-
-  :focus {
-    border-color: ${getColor('primary')};
-    box-shadow: 0 0 0 3px ${getColor('primary.20')};
-  }
-
-  ${modifier('success')(css`
-    border-color: ${getColor('success')};
-    :focus {
-      border-color: ${getColor('success')};
-      box-shadow: 0 0 0 3px ${getColor('success.20')};
-    }
-  `)};
-  ${modifier('error')(css`
-    border-color: ${getColor('error')};
-    :focus {
-      border-color: ${getColor('error')};
-      box-shadow: 0 0 0 3px ${getColor('error.20')};
-    }
-  `)};
-  ${modifier('large')(css`
-    ${fontSize('large')};
-    padding: 0 20px;
-    line-height: 2.67;
-  `)};
-  ${modifier('small')(css`
-    ${fontSize('small')};
-    line-height: 2;
-  `)};
-  :disabled {
-    cursor: not-allowed;
-    color: ${getColor('coolGrey.40')};
-    background: ${getColor('coolGrey.10')};
-    border-color: ${getColor('coolGrey.20')};
-    box-shadow: none;
-  }
-`;
-
-SelectTag.displayName = 'SelectTagSelect';
+import './Select.scss';
 
 const renderOptions = options =>
   options.reduce((acc, option) => {
@@ -92,11 +33,24 @@ const renderOptions = options =>
     return acc;
   }, []);
 
-const Select = props => (
-  <FormElement {...props}>
-    <SelectTag {...props}>{props.children || renderOptions(props.options)}</SelectTag>
-  </FormElement>
-);
+const Select = props => {
+  const { success, error, large, small, className, hideLabel, ...otherProps } = props;
+  return (
+    <FormElement {...props} className={className} hideLabel={hideLabel}>
+      <select
+        {...otherProps}
+        className={bemClass('Select', {
+          success,
+          error,
+          large,
+          small,
+        })}
+      >
+        {props.children || renderOptions(props.options)}
+      </select>
+    </FormElement>
+  );
+};
 
 Select.displayName = 'Select';
 
@@ -107,6 +61,12 @@ Select.defaultProps = {
 
 Select.propTypes = {
   children: PropTypes.any,
+  success: PropTypes.bool,
+  error: PropTypes.bool,
+  large: PropTypes.bool,
+  small: PropTypes.bool,
+  className: PropTypes.string,
+  hideLabel: PropTypes.bool,
   options: PropTypes.arrayOf(
     PropTypes.oneOfType([
       PropTypes.string,
