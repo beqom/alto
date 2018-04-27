cafbea00d5e8e24d7a50f269296007ce1c945888
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-04-27 23:16:17 +0200

    Add debounced state to editable cell in Table

--- src/alto-ui/Table/story.js
+++ src/alto-ui/Table/story.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { boolean, text } from '@storybook/addon-knobs';
 import { DateTime } from 'luxon';
+import { boolean, text } from '@storybook/addon-knobs';
 import centered from '@storybook/addon-centered';
 import Avatar from '../Avatar';
 
