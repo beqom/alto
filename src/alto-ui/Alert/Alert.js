21bdc58204433e960e68c83147129960e3921295
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-05-04 14:00:48 +0200

    Add Alert component

---
+++ src/alto-ui/Alert/Alert.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bemClass } from '../helpers/bem';
+import CheckCircleIcon from '../Icons/CheckCircle';
+import ExclamationTriangleIcon from '../Icons/ExclamationTriangle';
+import ExclamationCircleIcon from '../Icons/ExclamationCircle';
+import InfoCircleIcon from '../Icons/InfoCircle';
+import CloseIcon from '../Icons/Close';
+
+import './Alert.scss';
+
+const getIcon = (success, warning, danger) => {
+  if (success) return CheckCircleIcon;
+  if (warning) return ExclamationTriangleIcon;
+  if (danger) return ExclamationCircleIcon;
+  return InfoCircleIcon;
+};
+
+const Alert = ({ className, global, success, warning, error, show, onClose, children }) => {
+  if (show === false) return null;
+  const Icon = getIcon(success, warning, error);
+  return (
+    <div
+      className={bemClass(
+        'alert',
+        { global, success, warning, error, closable: !!onClose },
+        className
+      )}
+    >
+      <Icon className="alert__icon" />
+      <div className="alert__message">{children}</div>
+      {onClose && <CloseIcon className="alert__close" onClick={onClose} />}
+    </div>
+  );
+};
+
+Alert.displayName = 'Alert';
+
+Alert.defaultProps = {};
+
+Alert.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.any,
+  global: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  error: PropTypes.bool,
+  show: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
+export default Alert;
---
+++ src/alto-ui/Alert/story.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import centered from '@storybook/addon-centered';
+
+import Alert from './Alert';
+
+storiesOf('Alert', module)
+  .addDecorator(centered)
+  .addWithJSX('overview', () => (
+    <Alert
+      global={boolean('global', false)}
+      success={boolean('success', false)}
+      warning={boolean('warning', false)}
+      error={boolean('error', false)}
+      show={boolean('show', true)}
+      onClose={boolean('onClose', true) ? action('onClose') : undefined}
+    >
+      {text('children', 'Hello world!')}
+    </Alert>
+  ))
+  .addWithJSX('long text', () => (
+    <Alert
+      global={boolean('global', false)}
+      success={boolean('success', false)}
+      warning={boolean('warning', false)}
+      error={boolean('error', false)}
+      onClose={boolean('onClose', true) ? action('onClose') : undefined}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate nisi a erat blandit
+      interdum a non lorem. Sed congue ut nunc vitae malesuada. Pellentesque egestas placerat ante
+      ut porttitor. Nullam lacinia nunc nec libero malesuada, vel maximus quam luctus. Nullam eu
+      neque sapien. Cras vehicula molestie posuere. Suspendisse euismod consectetur semper.
+    </Alert>
+  ));
