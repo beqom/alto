3f05f7e6298949f07f156a6146e029f0f74f10f6
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-05-06 01:49:50 +0200

    improve sidenav panel design

--- src/alto-ui/SideNav/SideNavPanel.js
+++ src/alto-ui/SideNav/SideNavPanel.js
@@ -3,24 +3,43 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import Overlay from '../Overlay';
-import Button from '../Button';
+import VisuallyHidden from '../VisuallyHidden';
+import CloseIcon from '../Icons/Close';
 import { bemClass } from '../helpers/bem';
 
-const SideNavPanel = ({ open, children, title, onClose, closeFocusTargetId, className }) => (
-  <Overlay open={open} openFocusTargetId={`${closeFocusTargetId}__open`} onClose={onClose} closeFocusTargetId={closeFocusTargetId}>
-    <div className={bemClass('sidenav__panel', { open })} aria-hidden={!open}>
+const SideNavPanel = ({
+  open,
+  children,
+  title,
+  onClose,
+  closeFocusTargetId,
+  className,
+  panelClassName,
+}) => (
+  <Overlay
+    open={open}
+    openFocusTargetId={`${closeFocusTargetId}__open`}
+    onClose={onClose}
+    closeFocusTargetId={closeFocusTargetId}
+  >
+    <div className={bemClass('sidenav__panel', { open }, panelClassName)} aria-hidden={!open}>
       <div className="sidenav__panel-container">
         <header className="sidenav__panel-header">
-          <div className="sidenav__panel-title">
-            {title}
-          </div>
-          <Button tabIndex={open ? 0 : -1} id={`${closeFocusTargetId}__open`} outline small onClick={onClose}>
-            CLOSE
-          </Button>
+          <div className="sidenav__panel-title">{title}</div>
+          <button
+            id={`${closeFocusTargetId}__open`}
+            onClick={onClose}
+            className="sidenav__panel-close"
+            tabIndex={open ? 0 : -1}
+          >
+            <CloseIcon />
+            <div className="sidenav__panel-close-label">
+              <span aria-hidden="true">ESC</span>
+              <VisuallyHidden>Close {title} panel.</VisuallyHidden>
+            </div>
+          </button>
         </header>
-        <div className={classnames('sidenav__panel-body', className)}>
-          {children}
-        </div>
+        <div className={classnames('sidenav__panel-body', className)}>{children}</div>
       </div>
     </div>
   </Overlay>
@@ -28,11 +47,11 @@ const SideNavPanel = ({ open, children, title, onClose, closeFocusTargetId, clas
 
 SideNavPanel.displayName = 'SideNavPanel';
 
-SideNavPanel.defaultProps = {
-};
+SideNavPanel.defaultProps = {};
 
 SideNavPanel.propTypes = {
   className: PropTypes.string,
+  panelClassName: PropTypes.string,
   open: PropTypes.bool.isRequired,
   children: PropTypes.any.isRequired,
   title: PropTypes.string.isRequired,
