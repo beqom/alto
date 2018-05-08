049d68b4c3a9170bb1f3c0b5488b869f74a24a28
Andy McFee <andy.mcfee@gmail.com>
2018-05-08 09:19:30 +0200

    Clean up ViewColumns icon

--- src/alto-ui/Icons/ViewColumns.js
+++ src/alto-ui/Icons/ViewColumns.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Icon from './Icon';
 
-const Filter = props => (
+const ViewColumns = props => (
   <Icon {...props}>
     {ownProps => (
-      <g>
-        <path
-          {...ownProps}
-          d="M31,5H5A2,2,0,0,0,3,7V29a2,2,0,0,0,2,2H31a2,2,0,0,0,2-2V7A2,2,0,0,0,31,5ZM13,29H5V7h8Zm10,0H15V7h8Z"
-        />
-      </g>
+      <path
+        {...ownProps}
+        d="M31,5H5A2,2,0,0,0,3,7V29a2,2,0,0,0,2,2H31a2,2,0,0,0,2-2V7A2,2,0,0,0,31,5ZM13,29H5V7h8Zm10,0H15V7h8Z"
+      />
     )}
   </Icon>
 );
 
-Filter.displayName = 'Filter';
+ViewColumns.displayName = 'ViewColumns';
 
-export default Filter;
+export default ViewColumns;
