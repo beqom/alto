bd2f17d657408859d8884bb97f06f8dbcb98def7
Nicolas Martos <nicolas.martos@zeffyr.com>
2018-06-12 11:32:52 +0200

    Avatar : Add unit test

--- src/alto-ui/Avatar/Avatar.js
+++ src/alto-ui/Avatar/Avatar.js
@@ -8,22 +8,20 @@ import Image from '../Image';
 import './Avatar.scss';
 
 const Avatar = ({ className, src, alt, large, small, big }) => (
-  <div className={bemClass('avatar', { big, large, small }, className)}>
-    <Image className="avatar__image" {...{src, alt}}>
-      <div className="avatar__placeholder">
-        <div className="avatar__placeholder-icon-container">
-          <UserIcon className="avatar__placeholder-icon" />
+  <div className={bemClass('Avatar', { big, large, small }, className)}>
+    <Image className="Avatar__image" {...{ src, alt }}>
+      <div className="Avatar__placeholder">
+        <div className="Avatar__placeholder-icon-container">
+          <UserIcon className="Avatar__placeholder-icon" />
         </div>
       </div>
     </Image>
   </div>
 );
 
-
 Avatar.displayName = 'Avatar';
 
-Avatar.defaultProps = {
-};
+Avatar.defaultProps = {};
 
 Avatar.propTypes = {
   className: PropTypes.string,
