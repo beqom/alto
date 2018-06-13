411aa0711dccda1ac260fa2e54551164e2eb9315
Nicolas Martos <nicolas.martos@zeffyr.com>
2018-06-13 09:50:55 +0200

    Fix sonar issue on TextField

--- src/alto-ui/helpers/navigator.js
+++ src/alto-ui/helpers/navigator.js
@@ -1,3 +1,4 @@
+// https://gist.github.com/gaboratorium/25f08b76eb82b1e7b91b01a0448f8b1d
 export const isIE11 = () => {
   const { userAgent } = window.navigator;
   return userAgent.indexOf('Trident/') !== -1;
