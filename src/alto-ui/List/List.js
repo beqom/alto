fc2d9c03c2f193cec123c2b4e80f26aefe281403
David Jomain <kaynshin@gmail.com>
2018-06-13 10:07:33 +0200

    Add ID on buttons and links in List component

--- src/alto-ui/List/List.js
+++ src/alto-ui/List/List.js
@@ -7,23 +7,27 @@ import { bemClass } from '../helpers/bem';
 
 import './List.scss';
 
-const renderItemContent = (item, selected, render) => {
+const renderItemContent = (listId, item, selected, render) => {
   const active = typeof item === 'string' ? selected === item : selected === item.id;
   const link = !!item.url;
   const Component = link ? Link : 'div';
-  const props = link ? { href: item.url }  :{};
+  const props = link ? { href: item.url } : {};
   return (
-    <Component {...props} className={bemClass('list__item-content', { active, link })}>
+    <Component
+      {...props}
+      id={link ? `${listId}__list-item--${item.id}` : undefined}
+      className={bemClass('list__item-content', { active, link })}
+    >
       {render(item, active)}
     </Component>
   );
-}
+};
 
-const List = ({ items, children, selected, className }) => (
-  <ul className={classnames('list', className)}>
+const List = ({ id, items, children, selected, className }) => (
+  <ul id={id} className={classnames('list', className)}>
     {items.map(item => (
       <li key={item.id || item} className="list__item">
-        {renderItemContent(item, selected, children)}
+        {renderItemContent(id, item, selected, children)}
       </li>
     ))}
   </ul>
@@ -32,23 +36,26 @@ const List = ({ items, children, selected, className }) => (
 List.displayName = 'List';
 
 List.defaultProps = {
-  children: item =>  item.title || item.name || item,
+  children: item => item.title || item.name || item,
 };
 
 const idPropType = PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]);
 
 List.propTypes = {
+  id: PropTypes.string,
   className: PropTypes.string,
   children: PropTypes.func,
   selected: idPropType,
-  items: PropTypes.arrayOf(PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.shape({
-      id: idPropType.isRequired,
-      url: PropTypes.string,
-      title: PropTypes.string,
-    }),
-  ])).isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        id: idPropType.isRequired,
+        url: PropTypes.string,
+        title: PropTypes.string,
+      }),
+    ])
+  ).isRequired,
 };
 
 export default List;
