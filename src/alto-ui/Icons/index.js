46aae0dc0fd7e462976cdae17c59d8ff512d59e7
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-06-14 11:06:28 +0200

    Add tests for all icons

---
+++ src/alto-ui/Icons/index.js
@@ -0,0 +1,149 @@
+import Add from './Add';
+import ArrowDown from './ArrowDown';
+import ArrowLeft from './ArrowLeft';
+import ArrowRight from './ArrowRight';
+import ArrowUp from './ArrowUp';
+import Bars from './Bars';
+import Bell from './Bell';
+import Bolt from './Bolt';
+import Building from './Building';
+import Calculator from './Calculator';
+import Check from './Check';
+import CheckCircle from './CheckCircle';
+import ChevronDown from './ChevronDown';
+import ChevronLeft from './ChevronLeft';
+import ChevronRight from './ChevronRight';
+import ChevronUp from './ChevronUp';
+import Close from './Close';
+import Cog from './Cog';
+import Config from './Config';
+import Collapse from './Collapse';
+import Copy from './Copy';
+import Delete from './Delete';
+import Download from './Download';
+import Edit from './Edit';
+import EllipsisHorizontal from './EllipsisHorizontal';
+import EllipsisVertical from './EllipsisVertical';
+import Envelope from './Envelope';
+import ErrorIcon from './ErrorIcon';
+import ExclamationCircle from './ExclamationCircle';
+import ExclamationTriangle from './ExclamationTriangle';
+import Expand from './Expand';
+import ExportFile from './ExportFile';
+import Eye from './Eye';
+import File from './File';
+import FileGroup from './FileGroup';
+import FileSettings from './FileSettings';
+import Filter from './Filter';
+import Folder from './Folder';
+import FolderOpen from './FolderOpen';
+import Globe from './Globe';
+import Help from './Help';
+import HelpInfo from './HelpInfo';
+import Home from './Home';
+import Image from './Image';
+import ImportFile from './ImportFile';
+import InfoCircle from './InfoCircle';
+import Lightbulb from './Lightbulb';
+import List from './List';
+import Menu from './Menu';
+import Minus from './Minus';
+import More from './More';
+import Note from './Note';
+import MinusCircle from './MinusCircle';
+import Objects from './Objects';
+import Options from './Options';
+import Organization from './Organization';
+import Pencil from './Pencil';
+import Plus from './Plus';
+import PlusCircle from './PlusCircle';
+import Populations from './Populations';
+import Remove from './Remove';
+import Search from './Search';
+import Tasks from './Tasks';
+import Times from './Times';
+import Trash from './Trash';
+import Unknown from './Unknown';
+import Upload from './Upload';
+import User from './User';
+import ViewCards from './ViewCards';
+import ViewColumns from './ViewColumns';
+import ViewList from './ViewList';
+import World from './World';
+import Tool from './Tool';
+
+export default {
+  Add,
+  ArrowDown,
+  ArrowLeft,
+  ArrowUp,
+  ArrowRight,
+  Bars,
+  Bell,
+  Bolt,
+  Building,
+  Calculator,
+  Check,
+  CheckCircle,
+  ChevronDown,
+  ChevronLeft,
+  ChevronUp,
+  ChevronRight,
+  Close,
+  Cog,
+  Collapse,
+  Copy,
+  Config,
+  Delete,
+  Download,
+  Edit,
+  EllipsisHorizontal,
+  EllipsisVertical,
+  Envelope,
+  ErrorIcon,
+  ExclamationCircle,
+  ExclamationTriangle,
+  Expand,
+  ExportFile,
+  Eye,
+  File,
+  FileGroup,
+  FileSettings,
+  Filter,
+  Folder,
+  FolderOpen,
+  Globe,
+  Help,
+  HelpInfo,
+  Home,
+  Image,
+  ImportFile,
+  InfoCircle,
+  Lightbulb,
+  List,
+  Menu,
+  Minus,
+  MinusCircle,
+  More,
+  Note,
+  Objects,
+  Options,
+  Organization,
+  Pencil,
+  Plus,
+  PlusCircle,
+  Populations,
+  Remove,
+  Search,
+  Tasks,
+  Times,
+  Trash,
+  Unknown,
+  Upload,
+  User,
+  ViewCards,
+  ViewColumns,
+  ViewList,
+  World,
+  Tool,
+};
