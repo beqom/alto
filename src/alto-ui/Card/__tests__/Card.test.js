b3e070fd0c2394a8cf9bc7df67893e5b4a51295d
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-06-15 10:10:49 +0200

    Card: add unit tests

---
+++ src/alto-ui/Card/__tests__/Card.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Card from '../Card';
+
+describe('<Card />', () => {
+  it('should render correctly by default', () => {
+    const wrapper = shallow(<Card>Hello world</Card>);
+    expect(wrapper.type()).toBe('div');
+    expect(wrapper.children().length).toBe(1);
+    expect(wrapper.childAt(0).prop('children')).toBe('Hello world');
+  });
+
+  it('should support severals modifiers', () => {
+    ['small', 'large', 'filled'].forEach(modifier => {
+      const wrapper = shallow(<Card {...{ [modifier]: true }} />);
+      expect(wrapper.prop('className')).toBe(`Card Card--${modifier}`);
+    });
+  });
+
+  it('should be able to render a title', () => {
+    const wrapper = shallow(<Card title="Welcome to Aperture Science">My name is Glados</Card>);
+    expect(wrapper.children().length).toBe(2);
+    expect(wrapper.childAt(0).text()).toBe('Welcome to Aperture Science');
+    expect(wrapper.childAt(1).prop('children')).toBe('My name is Glados');
+  });
+});
