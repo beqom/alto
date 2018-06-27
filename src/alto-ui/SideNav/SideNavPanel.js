42633052f2745b263e245d3f97401fd35e233430
Nicolas Martos <nicolas.martos@zeffyr.com>
2018-06-27 10:03:43 +0200

    SideNavPanel : manage transition and remove panel from dom

--- src/alto-ui/SideNav/SideNavPanel.js
+++ src/alto-ui/SideNav/SideNavPanel.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
 import Overlay from '../Overlay';
 import CloseButton from '../CloseButton';
-import { bemClass } from '../helpers/bem';
 
 const SideNavPanel = ({
   open,
@@ -21,20 +22,26 @@ const SideNavPanel = ({
     onClose={onClose}
     closeFocusTargetId={closeFocusTargetId}
   >
-    <div className={bemClass('sidenav__panel', { open }, panelClassName)} aria-hidden={!open}>
-      <div className="sidenav__panel-container">
-        <header className="sidenav__panel-header">
-          <div className="sidenav__panel-title">{title}</div>
-          <CloseButton
-            id={`${closeFocusTargetId}__open`}
-            onClick={onClose}
-            tabIndex={open ? 0 : -1}
-            a11yLabel="Close Panel"
-          />
-        </header>
-        <div className={classnames('sidenav__panel-body', className)}>{children}</div>
-      </div>
-    </div>
+    <TransitionGroup>
+      {open && (
+        <CSSTransition classNames="sidenav__panel--transition" in={open} timeout={350}>
+          <div className="sidenav__panel">
+            <div className={classnames('sidenav__panel-container', panelClassName)}>
+              <header className="sidenav__panel-header">
+                <div className="sidenav__panel-title">{title}</div>
+                <CloseButton
+                  id={`${closeFocusTargetId}__open`}
+                  onClick={onClose}
+                  tabIndex={open ? 0 : -1}
+                  a11yLabel="Close Panel"
+                />
+              </header>
+              <div className={classnames('sidenav__panel-body', className)}>{children}</div>
+            </div>
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
   </Overlay>
 );
 
