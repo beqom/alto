8726825ca5d424f0237a938b2edcb5a0c27a76a0
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-07-20 18:10:16 +0200

    [Datagrid] total summury

--- src/alto-ui/Datagrid/components/DatagridRow/DatagridRow.js
+++ src/alto-ui/Datagrid/components/DatagridRow/DatagridRow.js
@@ -4,26 +4,37 @@ import PropTypes from 'prop-types';
 import DatagridCell from '../DatagridCell';
 import './DatagridRow.scss';
 
-const DatagridRow = ({ index, rowIndex, columns, columnIndexStart, row, context }) => (
-  <div role="row" aria-rowindex={index + 1} className="DatagridRow">
+const DatagridRow = ({
+  index,
+  rowIndex,
+  columns,
+  columnIndexStart,
+  row,
+  render,
+  header,
+  context,
+}) => (
+  <div role="row" aria-rowindex={rowIndex} className="DatagridRow">
     {columns.map((column, colIndex) => {
-      const editable = context.editable(column, row) && !column.formula;
-      const edited = context.edited(column, row, colIndex, index);
+      const editable = !render && context.editable(column, row) && !column.formula;
+      const edited = !render && context.edited(column, row, colIndex, index);
+      const id =
+        context.id && row
+          ? `${context.id}__cell--${column.key}-${context.rowKeyField(row)}`
+          : undefined;
       return (
         <DatagridCell
-          id={
-            context.id
-              ? `${context.id}__cell--${column.key}-${context.rowKeyField(row)}`
-              : undefined
-          }
+          id={id}
           key={column.key}
           row={row}
-          rowIndex={rowIndex + 1}
+          rowIndex={rowIndex}
           column={column}
-          colIndex={colIndex + columnIndexStart + 1}
-          context={context}
+          colIndex={colIndex + columnIndexStart}
           editable={editable}
           edited={edited}
+          render={render}
+          header={header}
+          context={context}
         />
       );
     })}
@@ -33,13 +44,15 @@ const DatagridRow = ({ index, rowIndex, columns, columnIndexStart, row, context 
 DatagridRow.displayName = 'DatagridRow';
 
 DatagridRow.defaultProps = {
-  columnIndexStart: 0,
+  columnIndexStart: 1,
+  index: 0,
 };
 
 DatagridRow.propTypes = {
-  row: PropTypes.object.isRequired,
+  row: PropTypes.object,
   rowIndex: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
+  index: PropTypes.number,
+  render: PropTypes.func,
   columns: PropTypes.arrayOf(
     PropTypes.shape({
       key: PropTypes.string.isRequired,
@@ -47,6 +60,7 @@ DatagridRow.propTypes = {
     }).isRequired
   ).isRequired,
   columnIndexStart: PropTypes.number,
+  header: PropTypes.bool,
   context: PropTypes.shape({
     id: PropTypes.string,
     rowKeyField: PropTypes.func.isRequired,
