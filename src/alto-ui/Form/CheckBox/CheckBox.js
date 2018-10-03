d8e078468b3ac4e56d8a62a9339f4c88eb0a88c8
Nicolas Martos <nicolas.martos@zeffyr.com>
2018-10-03 10:02:55 +0200

    Datagrid : add checkbox to manage selected rows

--- src/alto-ui/Form/CheckBox/CheckBox.js
+++ src/alto-ui/Form/CheckBox/CheckBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { bemClass } from '../../helpers/bem';
 
 import './CheckBox.scss';
 
 const CheckBox = props => {
-  const { label, ...inputProps } = props;
+  const { label, className, hideLabel, style, ...inputProps } = props;
   return (
-    <div className="CheckBox">
+    <div className={classnames('CheckBox', className)} style={style}>
       <input {...inputProps} className="CheckBox__input" type="checkbox" />
       <label
         htmlFor={props.id}
@@ -15,7 +17,7 @@ const CheckBox = props => {
         title={label}
         className="CheckBox__label"
       >
-        <span className="CheckBox__label-wrapper">{props.label}</span>
+        <div className={bemClass('CheckBox__label-text', { hidden: hideLabel })}>{props.label}</div>
       </label>
     </div>
   );
@@ -29,8 +31,11 @@ CheckBox.defaultProps = {
 CheckBox.propTypes = {
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object,
   checked: PropTypes.bool,
   disabled: PropTypes.bool,
+  hideLabel: PropTypes.bool,
 };
 
 export default CheckBox;
