b6c7ad46766f4cd999e01dbc0c74112276b246f5
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-10-04 14:59:20 +0200

    Add checkbox to dropdown + refactor (broke nested dropdown)

--- src/alto-ui/Dropdown/components/DropdownItem/DropdownItem.js
+++ src/alto-ui/Dropdown/components/DropdownItem/DropdownItem.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { bemClass } from '../../../helpers/bem';
-import Dropdown from '../../Dropdown';
+import Link from '../../../Link';
+import CheckBox from '../../../Form/CheckBox';
+import ChevronRightIcon from '../../../Icons/ChevronRight';
+import MoreIcon from '../../../Icons/More';
 
+import Dropdown from '../../Dropdown';
 import './DropdownItem.scss';
 
 const getPopoverProps = propoverProps => {
@@ -18,11 +22,17 @@ const getPopoverProps = propoverProps => {
   return { ...otherProps, right: true, start: true };
 };
 
+const renderMoreTriggerIcon = handleClick => (
+  <MoreIcon className="DropdownItem__more" onClick={handleClick} />
+);
+
 class DropdownItem extends React.Component {
   constructor() {
     super();
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.renderFullItem = this.renderFullItem.bind(this);
   }
 
   handleClick(e) {
@@ -37,23 +47,66 @@ class DropdownItem extends React.Component {
     this.props.onClose();
   }
 
-  render() {
-    const { item, dropdownProps, popoverProps } = this.props;
-    const { title, items, className, Icon, ...buttonProps } = item;
+  handleSelect() {
+    const { item, selected, dropdownProps } = this.props;
+    const { key } = item;
+    const selection = selected
+      ? dropdownProps.selected.filter(k => k !== key)
+      : [...dropdownProps.selected, key];
+    dropdownProps.onSelect(selection, item);
+  }
 
-    if (!items || !items.length) {
+  renderFullItem(...args) {
+    return <div className="DropdownItem">{this.renderItem(...args)}</div>;
+  }
+
+  renderItem(handleClick, active) {
+    const { item, selected, dropdownProps } = this.props;
+    const { key, title, className } = item;
+
+    if (dropdownProps.onSelect) {
       return (
-        <button
-          {...buttonProps}
-          onClick={this.handleClick}
-          className={bemClass('DropdownItem', { disabled: item.disabled }, className)}
-          title={title}
-        >
-          {Icon && <Icon left outline />}
-          {title}
-        </button>
+        <CheckBox
+          className={bemClass('DropdownItem__element', { checkbox: true }, className)}
+          id={`${dropdownProps.id || title}-checkbox-${key}`}
+          label={title}
+          checked={selected}
+          onChange={this.handleSelect}
+        />
       );
     }
+
+    return this.renderItemButton(handleClick, active);
+  }
+
+  renderItemButton(handleClick, active) {
+    const { item, selected, dropdownProps } = this.props;
+    const { title, items, onClick, className, Icon, ...itemProps } = item;
+    const hasItems = !!(items && items.length);
+    const hasOnClick = !!(onClick || dropdownProps.onClick);
+    const LinkOrButton = itemProps.href ? Link : 'button';
+
+    return (
+      <LinkOrButton
+        {...itemProps}
+        onClick={handleClick || this.handleClick}
+        className={bemClass(
+          'DropdownItem__element',
+          { button: true, disabled: item.disabled, active: active || item.active, selected },
+          className
+        )}
+        title={title}
+      >
+        {Icon && <Icon left outline />}
+        <span className="DropdownItem__element-title">{title}</span>
+        {hasItems && !hasOnClick && <ChevronRightIcon right />}
+      </LinkOrButton>
+    );
+  }
+
+  renderDropdown(renderTrigger) {
+    const { item, dropdownProps, popoverProps } = this.props;
+    const { items } = item;
 
     return (
       <Dropdown
@@ -61,22 +114,32 @@ class DropdownItem extends React.Component {
         {...getPopoverProps(popoverProps)}
         items={items}
         onClose={undefined}
-      >
-        {(toggle, open, close, isOpen) => (
-          <button
-            {...buttonProps}
-            className={bemClass(
-              'DropdownItem',
-              { subdropdown: true, active: isOpen, disabled: item.disabled },
-              className
-            )}
-            onClick={toggle}
-          >
-            {title}
-          </button>
-        )}
-      </Dropdown>
+        renderTrigger={renderTrigger}
+      />
     );
+  }
+
+  render() {
+    const {
+      item: { items, onClick },
+      dropdownProps,
+    } = this.props;
+
+    const hasItems = !!(items && items.length);
+    const hasOnClick = !!(onClick || dropdownProps.onClick);
+
+    if (hasItems) {
+      if (hasOnClick) {
+        return (
+          <div className="DropdownItem">
+            {this.renderItem()}
+            {this.renderDropdown(renderMoreTriggerIcon)}
+          </div>
+        );
+      }
+      return this.renderDropdown(this.renderFullItem);
+    }
+    return this.renderFullItem();
   }
 }
 
@@ -90,10 +153,12 @@ DropdownItem.propTypes = {
     items: PropTypes.array,
     className: PropTypes.string,
     onClick: PropTypes.func,
+    href: PropTypes.string,
   }).isRequired,
   dropdownProps: PropTypes.object.isRequired,
   popoverProps: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
+  selected: PropTypes.func,
 };
 
 export default DropdownItem;
