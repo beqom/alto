c156da1dfff7bfcece6b946f8fe0f00a6322edc0
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-11-16 08:41:37 +0100

    Create a <Input /> component

---
+++ src/alto-ui/Input/story.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import centered from '@storybook/addon-centered';
+
+import Input from './Input';
+
+const options = [
+  { title: 'Apple', value: 'apple' },
+  { title: 'Banana', value: 'banana' },
+  { title: 'Orange', value: 'orange' },
+];
+
+storiesOf('Input', module)
+  .addDecorator(centered)
+  .addWithJSX('playground', () => {
+    const type = select(
+      'type',
+      [
+        'number',
+        'integer',
+        'float',
+        'boolean',
+        'date',
+        'datetime',
+        'select',
+        'dropdown',
+        'text',
+        'textarea',
+      ],
+      'number'
+    );
+    return (
+      <Input
+        id="input"
+        type={type}
+        onChange={action('onChange')}
+        {...(['select', 'dropdown'].includes(type) ? { options } : undefined)}
+        label={text('label', 'my input')}
+      />
+    );
+  });
