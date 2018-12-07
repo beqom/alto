fddeecaf1e9f53337470a5944bf3ad395c1b20e4
Renaud Tertrais <renaud.tertrais@gmail.com>
2018-12-07 19:54:22 +0100

    Create a story for InputNumber

---
+++ src/alto-ui/Form/InputNumber/story.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import { text, boolean, number, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import InputNumber from './InputNumber';
+import StateProvider from '../../StateProvider';
+
+storiesOf('Form/InputNumber', module)
+  .addDecorator(centered)
+  .addWithJSX('playground', () => {
+    const value = text('value', '1500.048');
+    return (
+      <StateProvider state={{ value }}>
+        {(state, setState) => (
+          <InputNumber
+            id="input-number"
+            label="My number input"
+            value={state.value}
+            precision={number('precision', 2)}
+            locale={select('locale', ['en', 'fr', 'de', 'it', 'es', 'tr'], 'en')}
+            currency={text('currency', '$')}
+            onChange={(...args) => {
+              action('onChange')(...args);
+              setState({ value: args[1] });
+            }}
+            disableThousandSeparator={boolean('disableThousandSeparator', false)}
+          />
+        )}
+      </StateProvider>
+    );
+  });
--- src/alto-ui/StateProvider.js
+++ src/alto-ui/StateProvider.js
@@ -1,11 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const diff = (a, b) => {
+  const aEntries = Object.entries(a);
+  return (
+    aEntries.length !== Object.values(b).length ||
+    aEntries.some(([key, value]) => typeof value !== 'function' && value !== b[key])
+  );
+};
 
 class StateProvider extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = props.state;
+    this.state = {
+      __PREVIOUS_STATE__: props.state,
+      ...props.state,
+    };
     this.setState = this.setState.bind(this);
     this.refsMap = props.refs.reduce(
       (acc, refName) => ({ ...acc, [refName]: React.createRef() }),
@@ -13,10 +25,14 @@ class StateProvider extends React.Component {
     );
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.state !== this.props.state) {
-      this.setState(nextProps.state);
+  static getDerivedStateFromProps(props, state) {
+    if (diff(props.state, state.__PREVIOUS_STATE__)) {
+      return {
+        __PREVIOUS_STATE__: props.state,
+        ...props.state,
+      };
     }
+    return null;
   }
 
   render() {
