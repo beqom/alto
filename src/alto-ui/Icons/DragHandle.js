dbb7b66f58d0bcf8edeb0836fa75df5431f3c931
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2019-01-29 12:58:39 +0100

    Add new data for DragHandle icon

--- src/alto-ui/Icons/DragHandle.js
+++ src/alto-ui/Icons/DragHandle.js
@@ -4,13 +4,11 @@ import Icon from './Icon';
 const DragHandle = props => (
   <Icon {...props}>
     {ownProps => (
-      <g {...ownProps} transform="rotate(0, 18, 18)">
-        <circle cx="15" cy="12" r="1.5" />
-        <circle cx="15" cy="24" r="1.5" />
-        <circle cx="21" cy="12" r="1.5" />
-        <circle cx="21" cy="24" r="1.5" />
-        <circle cx="21" cy="18" r="1.5" />
-        <circle cx="15" cy="18" r="1.5" />
+      <g {...ownProps}>
+        <path
+          {...ownProps}
+          d="M11.94,21a3,3,0,1,1,3-3A3,3,0,0,1,11.94,21Zm12.12,0a3,3,0,1,1,3-3A3,3,0,0,1,24.06,21Zm0,12a3,3,0,1,1,3-3A3,3,0,0,1,24.06,33ZM24.06,9a3,3,0,1,1,3-3A3,3,0,0,1,24.06,9ZM11.94,33a3,3,0,1,1,3-3A3,3,0,0,1,11.94,33ZM11.94,9a3,3,0,1,1,3-3A3,3,0,0,1,11.94,9Z"
+        />
       </g>
     )}
   </Icon>
