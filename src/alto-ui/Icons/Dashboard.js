d9578d57bbc1798b572ec8a971dd3254b325a6b0
Nicolas Martos <nicolas.martos@zeffyr.com>
2019-02-20 12:19:09 +0100

    Dashboard Icon

--- src/alto-ui/Icons/Dashboard.js
+++ src/alto-ui/Icons/Dashboard.js
@@ -5,12 +5,15 @@ const Dashboard = props => (
   <Icon {...props}>
     {ownProps => (
       <g>
-        <path {...ownProps} d="M19,4v8H5V4Zm2-2H3V14H21V2Z" />
-        <path {...ownProps} d="M31,15V31H8V18H25V15Zm2-2H23v3H6V33H33V13Z" />
-        <polygon
-          {...ownProps}
-          points="31 6 31 8 29 8 29 10 27 10 27 8 25 8 25 6 27 6 27 4 29 4 29 6 31 6"
-        />
+        <rect {...ownProps} x="2.9" y="3.01" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="12.65" y="3.01" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="22.4" y="3.01" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="2.9" y="12.65" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="12.65" y="12.65" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="22.4" y="12.65" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="2.9" y="22.3" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="12.65" y="22.3" width="7.3" height="7.3" rx="1.42" />
+        <rect {...ownProps} x="22.4" y="22.3" width="7.3" height="7.3" rx="1.42" />
       </g>
     )}
   </Icon>
