7e6455d8abb1f818331b25209257dc8df1c26d1c
Renaud Tertrais <renaud.tertrais@gmail.com>
2019-03-19 09:05:07 +0100

    Add Tag component

---
+++ src/alto-ui/Tag/story.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered';
+import { action } from '@storybook/addon-actions';
+
+import Tag from './Tag';
+
+storiesOf('Tag', module)
+  .addDecorator(centered)
+  .addWithJSX('overview', () => (
+    <Tag
+      active={boolean('active', false)}
+      rounded={boolean('rounded', false)}
+      onClick={boolean('onClick', false) ? action('onClick') : undefined}
+      disabled={boolean('disabled', false)}
+    >
+      {text('children', 'My Tag')}
+    </Tag>
+  ));
