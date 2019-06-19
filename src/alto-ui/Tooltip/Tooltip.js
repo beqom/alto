703409eeef57a1bab06ddc843bf9480901d9364f
Renaud Tertrais <renaud.tertrais@gmail.com>
2019-06-19 17:11:09 +0200

    Refactor Tooltip with hooks

--- src/alto-ui/Tooltip/Tooltip.js
+++ src/alto-ui/Tooltip/Tooltip.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { bemClass } from '../helpers/bem';
@@ -19,123 +19,99 @@ const getIcon = ({ info, success, warning, error }) => {
   return null;
 };
 
-class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
+function Tooltip({
+  className,
+  children,
+  content,
+  info,
+  success,
+  error,
+  warning,
+  narrow,
+  wide,
+  show: shouldBeVisible,
+  big,
+  ...relativeBoxProps
+}) {
+  const [visible, setVisibility] = useState(shouldBeVisible);
+  const defaultRef = useRef();
+  const targetRef = relativeBoxProps.targetRef || defaultRef;
 
-    this.state = {
-      // eslint-disable-next-line react/no-unused-state
-      show: props.show,
-      visible: props.show,
-    };
+  const show = () => setVisibility(true);
+  const hide = () => setVisibility(false);
 
-    this.wrapperRef = React.createRef();
+  useEffect(() => {
+    setVisibility(shouldBeVisible);
+  }, [shouldBeVisible]);
 
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-  }
-
-  static getDerivedStateFromProps({ show }, state) {
-    if (show !== state.show) {
-      return {
-        show,
-        visible: show,
-      };
+  useEffect(() => {
+    if (targetRef.current) {
+      targetRef.current.addEventListener('mouseenter', show);
+      targetRef.current.addEventListener('mouseleave', hide);
     }
-    return null;
-  }
+    return () => {
+      if (targetRef.current) {
+        targetRef.current.removeEventListener('mouseenter', show);
+        targetRef.current.removeEventListener('mouseleave', hide);
+      }
+    };
+  }, [targetRef]);
 
-  handleMouseEnter() {
-    this.setState({ visible: true });
-  }
+  const Icon = getIcon({
+    info,
+    success,
+    error,
+    warning,
+  });
 
-  handleMouseLeave() {
-    this.setState({ visible: false });
-  }
-
-  renderTooltip() {
-    const {
-      className,
-      children,
-      content,
-      info,
-      success,
-      error,
-      warning,
-      narrow,
-      wide,
-      show,
-      big,
-      ...relativeBoxProps
-    } = this.props;
-
-    const { visible } = this.state;
-
-    const Icon = getIcon({
-      info,
-      success,
-      error,
-      warning,
-    });
-
-    if (!visible) return null;
-
-    return (
-      <Portal>
-        <RelativeBox
-          className={bemClass(
-            'Tooltip',
-            {
+  const tooltip = !visible ? null : (
+    <Portal>
+      <RelativeBox
+        className={bemClass(
+          'Tooltip',
+          {
+            info,
+            success,
+            error,
+            warning,
+            narrow,
+            wide,
+            big,
+            visible,
+          },
+          className
+        )}
+        baseClassName="Tooltip"
+        bottom
+        middle
+        target={children ? this.wrapperRef.current : undefined}
+        margin={6.4}
+        {...relativeBoxProps}
+      >
+        {Icon && (
+          <Icon
+            left
+            baseline
+            className={bemClass('Tooltip__icon', {
               info,
               success,
               error,
               warning,
-              narrow,
-              wide,
-              big,
-              visible,
-            },
-            className
-          )}
-          baseClassName="Tooltip"
-          bottom
-          middle
-          target={children ? this.wrapperRef.current : undefined}
-          margin={6.4}
-          {...relativeBoxProps}
-        >
-          {Icon && (
-            <Icon
-              left
-              baseline
-              className={bemClass('Tooltip__icon', {
-                info,
-                success,
-                error,
-                warning,
-              })}
-            />
-          )}
-          <div className="Tooltip__content">{content}</div>
-        </RelativeBox>
-      </Portal>
-    );
-  }
+            })}
+          />
+        )}
+        <div className="Tooltip__content">{content}</div>
+      </RelativeBox>
+    </Portal>
+  );
 
-  render() {
-    if (!this.props.children) return this.renderTooltip();
-    return (
-      <div
-        ref={this.wrapperRef}
-        className="Tooltip__wrapper"
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        {this.props.children}
-        {this.renderTooltip()}
-      </div>
-    );
-  }
+  if (!children) return tooltip;
+  return (
+    <div ref={targetRef} className="Tooltip__wrapper">
+      {children}
+      {tooltip}
+    </div>
+  );
 }
 
 Tooltip.displayName = 'Tooltip';
