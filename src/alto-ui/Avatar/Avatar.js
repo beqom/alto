922513f7986017294b5150106658d33434d0df6f
Kevin Silva <kevin.silva@beqom.com>
2019-10-31 16:05:47 +0100

    Avatar accepts now a custom icon

--- src/alto-ui/Avatar/Avatar.js
+++ src/alto-ui/Avatar/Avatar.js
@@ -8,19 +8,16 @@ import Image from '../Image';
 import './Avatar.scss';
 import Tooltip from '../Tooltip';
 
-function Avatar({ className, src, alt, tooltip, large, small, big, getDefaultAvatarIcon }) {
+function Avatar({ className, src, alt, tooltip, large, small, big, icon }) {
   const ref = useRef();
+  const Icon = icon || UserIcon;
 
   return (
     <div ref={ref} className={bemClass('Avatar', { big, large, small }, className)}>
       <Image className="Avatar__image" src={src} alt={alt || tooltip}>
         <div className="Avatar__placeholder">
           <div className="Avatar__placeholder-icon-container">
-            {getDefaultAvatarIcon && typeof getDefaultAvatarIcon === 'function' ? (
-              getDefaultAvatarIcon()
-            ) : (
-              <UserIcon className="Avatar__placeholder-icon" />
-            )}
+            <Icon className="Avatar__placeholder-icon" />
           </div>
         </div>
       </Image>
@@ -41,7 +38,7 @@ Avatar.propTypes = {
   big: PropTypes.bool,
   large: PropTypes.bool,
   small: PropTypes.bool,
-  getDefaultAvatarIcon: PropTypes.func,
+  icon: PropTypes.any,
 };
 
 export default Avatar;
