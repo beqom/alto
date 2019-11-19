5649a63a6f10c420e63584269faa1cd06b41f2a9
Damian Iwanski <damian.iwanski@beqom.com>
2019-11-19 15:48:42 +0100

    Refactor Datagrid and create DataGridHead and DataGridContent

--- src/alto-ui/Datagrid/constants.js
+++ src/alto-ui/Datagrid/constants.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import { DateTime } from 'luxon';
+import { format as formatNumber } from '../helpers/number';
+import Avatar from '../Avatar';
+import Tooltip from '../Tooltip';
+import ErrorIcon from '../Icons/ErrorIcon';
+
 export const DEFAULT_LABELS = {
   errorFormula: 'There is an error in formula',
   a11ySortLabel: 'Click to sort this column by Ascending or Descending',
@@ -16,4 +23,48 @@ export const DATAGRID_INITIAL_STATE_RESIZER = {
   target: emptyBoundingClientRect,
   container: emptyBoundingClientRect,
   resizing: false,
+};
+
+export const PARSERS = {
+  date: x => (x ? new Date(x) : ''),
+  datetime: x => (x ? new Date(x) : ''),
+  boolean: x => {
+    const bool = `${x}`.toLowerCase();
+    if (['true', '1'].includes(bool)) return true;
+    if (['false', '0'].includes(bool)) return false;
+    return x;
+  },
+};
+
+export const FORMATTERS = {
+  date: (x, col, row, { locale }) =>
+    x instanceof Date
+      ? DateTime.fromJSDate(x)
+          .setLocale(locale)
+          .toFormat('dd LLL yyyy')
+      : '',
+  datetime: (x, col, row, { locale }) =>
+    x instanceof Date
+      ? DateTime.fromJSDate(x)
+          .setLocale(locale)
+          .toFormat('dd LLL yyyy hh:mm a')
+      : '',
+  number: (x, col, row, { locale }) =>
+    formatNumber(x, locale, col.precision || 0, null, col.disableThousandSeparator, {
+      percent: col.percent,
+    }),
+  integer: (...args) => FORMATTERS.number(...args),
+  float: (...args) => FORMATTERS.number(...args),
+  int: (...args) => FORMATTERS.number(...args),
+};
+
+export const RENDERERS = {
+  image: (x, col, row, { comfortable, compact }) => (
+    <Avatar small={compact} large={comfortable} src={x || ''} alt={col.title} />
+  ),
+  error: x => (
+    <Tooltip content={x.message}>
+      <ErrorIcon outline />
+    </Tooltip>
+  ),
 };
