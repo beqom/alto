5cb19a5994c5ac91b9fe36d3ea0266e1ea34c4d4
krolwofficial <krolwofficial@gmail.com>
2019-11-27 16:08:28 +0100

    fixes after code review

--- src/alto-ui/Actions/__tests__/Actions.test.js
+++ src/alto-ui/Actions/__tests__/Actions.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Actions from '../Actions';
-
 import Dropdown from '../../Dropdown';
 import Group from '../../Group';
 
@@ -18,33 +17,33 @@ jest.mock('../../Group', () => {
   };
 });
 
-describe('<Actions />', () => {
-    const defaultProps = {
-      className: 'classTest',
-      id: 'id',
-      items: [
-        {
-          icon: null,
-          key: '1',
-          onClick: () => [],
-          title: 'Edit',
-        },
-        {
-          icon: null,
-          key: '2',
-          onClick: () => [],
-          title: 'Edit',
-        },
-      ],
-      max: 1
-    };
+describe('Actions', () => {
+  const defaultProps = {
+    className: 'classTest',
+    id: 'id',
+    items: [
+      {
+        icon: null,
+        key: '1',
+        onClick: jest.fn(),
+        title: 'Edit',
+      },
+      {
+        icon: null,
+        key: '2',
+        onClick: jest.fn(),
+        title: 'Edit',
+      },
+    ],
+    max: 1
+  };
 
   const getWrapper = ({ ...props }) => shallow(<Actions {...defaultProps} {...props} />);
 
   it('should not render if no items are provided', () => {
-    expect(getWrapper({items: undefined}).type()).toBe(null);
-    expect(getWrapper({items: null}).type()).toBe(null);
-    expect(getWrapper({items: []}).type()).toBe(null);
+    expect(getWrapper({items: undefined}).type()).toBeNull();
+    expect(getWrapper({items: null}).type()).toBeNull();
+    expect(getWrapper({items: []}).type()).toBeNull();
   });
 
   it('should render Dropdown if more items than max value', () => {
@@ -53,12 +52,6 @@ describe('<Actions />', () => {
 
     expect(DropdownMock).toHaveLength(1);
   });
-
-    it('should return undefined if onClick props is not a function', () => {
-      const wrapper = getWrapper({});
-      const DropdownMock = wrapper.find(Dropdown);
-      expect(DropdownMock.props().onClick).toBeUndefined();
-    });
 
   it('should render Group if less items than max value', () => {
     const wrapper = getWrapper({items: [{key: '1'}, {key: '2'}], max: 3 });
