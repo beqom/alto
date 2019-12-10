dcb56475481d4f3906f3b55af9377a36a305a3f0
krolwofficial <krolwofficial@gmail.com>
2019-12-10 09:04:32 +0100

    Added one test

--- src/alto-ui/Badge/__tests__/Badge.test.js
+++ src/alto-ui/Badge/__tests__/Badge.test.js
@@ -4,16 +4,21 @@ import { shallow } from 'enzyme';
 import Badge from '../Badge';
 
 describe('Badge', () => {
-
   it('should render without error', () => {
     const wrapper = shallow(<Badge />);
-    expect(wrapper.find('.Badge')).toHaveLength(1)
+    expect(wrapper.find('.Badge')).toHaveLength(1);
+  });
+
+  it('should pass id props', () => {
+    const wrapper = shallow(<Badge />);
+    wrapper.setProps({ id: 'test' });
+    expect(wrapper.find({ id: 'test' })).toHaveLength(1);
   });
 
   it('should return color classNames from color named props', () => {
     const props = {
-      red: true
-    }
+      red: true,
+    };
 
     const wrapper = shallow(<Badge {...props} />);
     expect(wrapper.hasClass('Badge')).toBe(true);
@@ -23,14 +28,14 @@ describe('Badge', () => {
   it('should update className when color props change', () => {
     const props = {
       red: true,
-      blue: false
-    }
+      blue: false,
+    };
     const wrapper = shallow(<Badge {...props} />);
     expect(wrapper.hasClass('Badge--red')).toBe(true);
 
     wrapper.setProps({
       red: false,
-      blue: true
+      blue: true,
     });
     expect(wrapper.hasClass('Badge--blue')).toBe(true);
   });
