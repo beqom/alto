c4fa4de0947790081ef9d6460a40bd2dc8b12555
krolwofficial <krolwofficial@gmail.com>
2019-12-18 14:38:34 +0100

    Added tests for Tag component

---
+++ src/alto-ui/Tag/__tests__/Tag.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Tag from './../Tag';
+
+describe('Tag', () => {
+  const getWrapper = props => shallow(<Tag {...props} />);
+
+  const defaultProps = {
+    className: 'className',
+    active: true,
+    rounded: true,
+    propMock: true,
+    onClick: jest.fn(),
+    disabled: true,
+  };
+
+  it('should render without error', () => {
+    const wrapper = getWrapper({});
+
+    expect(wrapper.find('div')).toHaveLength(1);
+    expect(wrapper.find('div').hasClass('Tag')).toBe(true);
+  });
+
+  it('should render button if passed onClick prop', () => {
+    const wrapper = getWrapper(defaultProps);
+
+    expect(wrapper.find('button')).toHaveLength(1);
+  });
+
+  it('should set correct class names', () => {
+    const wrapper = getWrapper(defaultProps);
+
+    const button = wrapper.find('button');
+
+    expect(button.hasClass('className')).toBe(true);
+    expect(button.hasClass('Tag--active')).toBe(true);
+    expect(button.hasClass('Tag--button')).toBe(true);
+    expect(button.hasClass('Tag--disabled')).toBe(true);
+    expect(button.hasClass('Tag--rounded')).toBe(true);
+  });
+});
