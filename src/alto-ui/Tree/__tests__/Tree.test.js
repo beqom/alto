bd63fa8c753d981fb81be36800b60910631cebef
krolwofficial <krolwofficial@gmail.com>
2020-01-14 10:21:35 +0100

    Refactor after code review

--- src/alto-ui/Tree/__tests__/Tree.test.js
+++ src/alto-ui/Tree/__tests__/Tree.test.js
@@ -1,45 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Tree from '../Tree'
+import Tree from '../Tree';
 
 import { getKey } from './../helpers';
 
 jest.mock('./../helpers', () => ({
-	getKey: jest.fn(),
+  getKey: jest.fn(({ key }) => key),
 }));
 
 describe('Tree', () => {
-    beforeEach(() => {
-        jest.resetAllMocks()
-        getKey.mockReturnValue('test')
+  beforeEach(() => {
+    getKey.mockClear();
+  });
+
+  const defaultProps = {
+    id: 'id',
+    items: [{ key: 'key_1' }, { key: 'key_2' }],
+  };
+
+  const getWrapper = props => shallow(<Tree {...defaultProps} {...props} />);
+
+  it('should render without error', () => {
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('.Tree').exists()).toBe(true);
+  });
+
+  describe('getKey func', () => {
+    it('should be called 4 times', () => {
+      getWrapper();
+
+      expect(getKey).toHaveBeenCalledTimes(4);
     });
 
-    const defaultProps = {
-        id: 'id',
-        items: [{}],
-    }
-    
-    const getWrapper = props => shallow(<Tree {...defaultProps} {...props} />);
-    
-    it('should render without error',() => {
-        const wrapper = getWrapper({});
-        
-        expect(wrapper.find('.Tree').exists()).toBe(true);
-    })
+    it('should pass formated value to TreeItem id value', () => {
+      const wrapper = getWrapper();
 
-    describe('getKey func', () => {
-        it('should pass formated value to TreeItem id value',() => {
-    
-            const wrapper = getWrapper({});
-                    
-            expect(wrapper.find('#id__item--test-0').key()).toEqual('test')
-        })
-    
-        it('should pass formated value to TreeItem key value',() => {    
-            const wrapper = getWrapper({});
-                    
-            expect(wrapper.find('#id__item--test-0').key()).toEqual('test')
-        })
-    })
-})
+      expect(wrapper.find('#id__item--key_1-0')).toHaveLength(1);
+      expect(wrapper.find('#id__item--key_2-1')).toHaveLength(1);
+    });
+
+    it('should pass formated value to TreeItem key value', () => {
+      const wrapper = getWrapper();
+
+      expect(wrapper.find('#id__item--key_1-0').key()).toBe('key_1');
+      expect(wrapper.find('#id__item--key_2-1').key()).toBe('key_2');
+    });
+  });
+});
--- src/alto-ui/Tree/components/TreeItem/__tests__/TreeItem.test.js
+++ src/alto-ui/Tree/components/TreeItem/__tests__/TreeItem.test.js
@@ -1,294 +1,378 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import TreeItem from '../TreeItem'
+import TreeItem from '../TreeItem';
 
 import { getKey } from '../../../helpers';
 
 jest.mock('../../../helpers', () => ({
-	getKey: jest.fn(),
+  getKey: jest.fn(),
 }));
 
-jest.mock('../../../../Icons/ChevronRight', () =>  {
-    const ChevronRight = () => <div className="ChevronRight" />;
-    return ChevronRight;
-});
+jest.mock('../../../../Icons/ChevronRight', () => () => <div className="ChevronRight" />);
 
 describe('TreeItem', () => {
-    beforeEach(() => jest.resetAllMocks());
+  const hasChildren = jest.fn();
+  beforeEach(() => jest.resetAllMocks());
+  const defaultProps = {
+    hasChildren,
+    state: {
+      open: true,
+      fetching: true,
+      children: [],
+    },
+    renderItem: jest.fn(),
+  };
 
-    const getWrapper = (defaultProps, props) => mount(<TreeItem {...defaultProps} {...props} />);
+  const getWrapper = props => mount(<TreeItem {...defaultProps} {...props} />);
 
-    describe('it', () => {
-        const defaultProps = {
-            hasChildren: jest.fn(),
-                state: {
-                    open: true,
-                    fetching: true,
-                    children: [],
-                },
-                renderItem: jest.fn(),
-        }
-            
-        it('should render without error',() => {
-            const wrapper = getWrapper(defaultProps, {})
-            
-            expect(wrapper.find('.TreeItem').exists()).toBe(true);
-            expect(wrapper.find('.TreeItem__title').exists()).toBe(true);
+  it('should render without error', () => {
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('.TreeItem').exists()).toBe(true);
+    expect(wrapper.find('.TreeItem__title').exists()).toBe(true);
+  });
+
+  it('should render TreeItem__title--final element', () => {
+    const obj = {
+      key: 'val',
+    };
+    const wrapper = getWrapper({ item: obj });
+
+    expect(wrapper.find('.TreeItem__title--final').exists()).toBe(true);
+    expect(hasChildren).toHaveBeenCalledWith(obj);
+  });
+
+  it('should render TreeItem__spinner element', () => {
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('.TreeItem__spinner').exists()).toBe(true);
+  });
+
+  describe('TreeItem__toggle-button element', () => {
+    const getCurrentWrapper = props => {
+      const { state: stateProps } = defaultProps;
+      return getWrapper({
+        ...defaultProps,
+        state: {
+          ...stateProps,
+          fetching: false,
+        },
+        hasChildren: jest.fn().mockReturnValue(true),
+        ...props,
+      });
+    };
+
+    it('should render without error', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.TreeItem__toggle-button')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render with open className', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.TreeItem__toggle-button--open')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render right id', () => {
+      expect(
+        getCurrentWrapper({ id: 'id' })
+          .find('#id__toggle-button')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render ChevronRightIcon element', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.ChevronRight')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should call handleToggle prop after click', () => {
+      const handleToggle = jest.fn();
+
+      const wrapper = getCurrentWrapper({
+        handleToggle: handleToggle(),
+      });
+      const button = wrapper.find('.TreeItem__toggle-button').at(0);
+
+      button.simulate('click');
+
+      expect(handleToggle).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Icon', () => {
+    const { state: stateProps } = defaultProps;
+    const getCurrentWrapper = props => {
+      return getWrapper({
+        ...defaultProps,
+        state: {
+          ...stateProps,
+        },
+        renderIcon: jest.fn().mockReturnValue(() => <div />),
+        ...props,
+      });
+    };
+
+    it('should render null', () => {
+      expect(
+        getCurrentWrapper({ renderIcon: null })
+          .find('.TreeItem__icon')
+          .exists()
+      ).toBe(false);
+      expect(
+        getCurrentWrapper({ renderIcon: undefined })
+          .find('.TreeItem__icon')
+          .exists()
+      ).toBe(false);
+    });
+    it('should render without error', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.TreeItem__icon')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render with outline prop', () => {
+      getKey.mockReturnValue([]);
+      
+      expect(
+        getCurrentWrapper({ selected: {} })
+          .find('.TreeItem__icon')
+          .children()
+          .prop('outline')
+      ).toBe(true);
+    });
+
+    it('should render without outline prop', () => {
+      const obj = {key: 'val'}
+
+      expect(
+        getCurrentWrapper({ selected: obj, item:obj })
+          .find('.TreeItem__icon')
+          .children()
+          .prop('outline')
+      ).toBe(false);
+
+      getKey.mockReturnValue(obj);
+      expect(
+        getCurrentWrapper({ selected: obj })
+          .find('.TreeItem__icon')
+          .children()
+          .prop('outline')
+      ).toBe(false);
+    });
+      
+    it('should call getKey with right value', () => {
+        const props = {
+          item: { key: 'value' },
+          keyField: '1',
+        };
+        getCurrentWrapper(props);
+
+        expect(getKey).toHaveBeenCalledWith({ key: 'value' }, '1');
+      })
+  });
+
+  describe('ButtonOrLink', () => {
+    const getCurrentWrapper = props => (getWrapper({ isClickable: true, ...props }))
+
+    it('should render as Link element', () => {
+      const wrapper = getCurrentWrapper({
+        href: jest.fn().mockReturnValue('link'),
+      });
+
+      expect(wrapper.find('Link').exists()).toBe(true);
+    });
+
+    it('should render as button element', () => {
+      const wrapper = getCurrentWrapper({
+        href: 'href',
+      });
+
+      expect(wrapper.find('button').exists()).toBe(true);
+    });
+
+    it('should render with href attribute', () => {
+      const hrefMock = jest.fn().mockReturnValue('link');
+      const item = {key: 'val'};
+      const selected = {key: 'val'};
+      getCurrentWrapper({
+        href: hrefMock,
+        item,
+        selected,
+      });
+
+      expect(hrefMock).toHaveBeenCalledTimes(1);
+      expect(hrefMock).toHaveBeenCalledWith(item, selected);
+    });
+
+    it('should render button without error', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.TreeItem__button')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render with id attribute', () => {
+      expect(
+        getCurrentWrapper({ id: 'id' })
+          .find('#id__button')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render with href attribute', () => {
+      const wrapper = getCurrentWrapper({
+        href: jest.fn().mockReturnValue('someLink'),
+      });
+
+      expect(
+        wrapper
+          .find('.TreeItem__button')
+          .first()
+          .find('a')
+          .prop('href')
+      ).toBe('someLink');
+    });
+
+    it('should render with active className', () => {
+      const obj = { foo: 'bar' };
+      const wrapper = getCurrentWrapper({
+        selected: obj,
+        item: obj,
+      });
+
+      expect(wrapper.find('.TreeItem__button--active').exists()).toBe(true);
+
+      getKey.mockReturnValue('test');
+
+      wrapper.setProps({ selected: 'test' });
+      wrapper.update();
+
+      expect(wrapper.find('.TreeItem__button--active').exists()).toBe(true);
+    });
+
+    it('should call renderItem function', () => {
+      const mockRenderItem = jest.fn();
+      const mockRenderItemTwo = jest.fn();
+
+      const wrapper = getCurrentWrapper({
+        renderItem: mockRenderItem,
+      });
+
+      expect(mockRenderItem).toHaveBeenCalledTimes(1);
+
+      wrapper.setProps({
+        renderItem: mockRenderItemTwo,
+      });
+
+      wrapper.update();
+
+      expect(mockRenderItemTwo).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('renderChildren', () => {
+    const { state: stateProps } = defaultProps;
+    const getCurrentWrapper = props => {
+      return getWrapper({
+        ...defaultProps,
+        state: {
+          ...stateProps,
+          children: [{}],
+        },
+        isClickable: true,
+        id: 'testId',
+        loadMore: jest.fn(),
+        labels: {
+          loadMore: 'test_label',
+        },
+        childrenPerPage: 1,
+        ...props,
+      });
+    };
+
+    it('should render without error', () => {
+      expect(
+        getCurrentWrapper()
+          .find('.TreeItem__subtree')
+          .exists()
+      ).toBe(true);
+    });
+
+    it('should render null', () => {
+      getKey.mockReturnValue(
+        Math.random()
+          .toString(36)
+          .substr(2, 9)
+      );
+
+      expect(
+        getCurrentWrapper({
+          state: {
+            open: false,
+          },
         })
-    
-        it('should render TreeItem__title with final className',() => {
-            const wrapper = getWrapper(defaultProps, {})
-                
-            expect(wrapper.find('.TreeItem__title--final').exists()).toBe(true);
+          .find('.TreeItem__subtree')
+          .exists()
+      ).toBe(false);
+
+      expect(
+        getCurrentWrapper({
+          state: {
+            children: [],
+          },
         })
-    
-        it('should render TreeItem__spinner element',() => {
-            const wrapper = getWrapper(defaultProps, {})
-                
-            expect(wrapper.find('.TreeItem__spinner').exists()).toBe(true);
+          .find('.TreeItem__subtree')
+          .exists()
+      ).toBe(false);
+
+      expect(
+        getCurrentWrapper({
+          state: {
+            children: null,
+          },
         })
-    })
+          .find('.TreeItem__subtree')
+          .exists()
+      ).toBe(false);
+    });
 
-    describe('TreeItem__toggle-button element', () => {
-        const defaultProps = {
-            state: {
-                open: true,
-                fetching: false,
-                children: [],
-            },
-            renderItem: jest.fn(),
-        }
+    it('should render Button element', () => {
+      const wrapper = getCurrentWrapper();
 
-        it('should render without error',() => {
-            const props = {
-                hasChildren: jest.fn().mockReturnValue(true),
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-                
-            expect(wrapper.find('.TreeItem__toggle-button').exists()).toBe(true);
-        })
+      expect(wrapper.find('.TreeItem__button-load-more').exists()).toBe(true);
+      expect(wrapper.find('#testId__load-more').exists()).toBe(true);
+      expect(
+        wrapper
+          .find('.TreeItem__button-load-more')
+          .at(0)
+          .text()
+      ).toBe('test_label');
+    });
 
-        it('should render with open className',() => {
-            const props = {
-                hasChildren: jest.fn().mockReturnValue(true),
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-                
-            expect(wrapper.find('.TreeItem__toggle-button--open').exists()).toBe(true);
-        })
+    it('should call loadMore function after Button is clicked', () => {
+      const mockLoadMore = jest.fn();
+      const button = getCurrentWrapper({
+        loadMore: mockLoadMore,
+      })
+        .find('.TreeItem__button-load-more')
+        .at(0);
 
-        it('should render proper id',() => {
-            const props = {
-                id: 'id',
-                hasChildren: jest.fn().mockReturnValue(true),
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-                
-            expect(wrapper.find('#id__toggle-button').exists()).toBe(true);
-        })
+      button.simulate('click');
 
-        it('should render ChevronRightIcon element',() => {
-            const props = {
-                hasChildren: jest.fn().mockReturnValue(true),
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-                
-            expect(wrapper.find('.ChevronRight').exists()).toBe(true);
-        })
-
-        it('should call handleToggle prop after click',() => {
-            const handleToggle = jest.fn()
-
-            const props = {
-                hasChildren: jest.fn().mockReturnValue(true),
-                handleToggle: handleToggle(),
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-            const button = wrapper.find('.TreeItem__toggle-button').at(0)
-
-            button.simulate('click')
-            
-            expect(handleToggle).toHaveBeenCalledTimes(1);
-        })
-     })
-    
-    describe('ButtonOrLink', () => {
-        const defaultProps = {
-            hasChildren: jest.fn(),
-                state: {
-                    open: true,
-                    fetching: true,
-                    children: [],
-                },
-                renderItem: jest.fn(),
-                isClickable: true,
-        }
-            
-        it('should render as Link element',() => {
-            const props = {
-                href: jest.fn().mockReturnValue('link'),
-            }
-
-            const wrapper = getWrapper(defaultProps, props)
-            
-            expect(wrapper.find('Link').exists()).toBe(true);
-        })
-
-        it('should render as button element',() => {
-            const props = {
-                href: 'href',
-            }
-
-            const wrapper = getWrapper(defaultProps, props)
-            
-            expect(wrapper.find('button').exists()).toBe(true);
-        })
-
-        it('should render without error',() => {
-            const wrapper = getWrapper(defaultProps, {})
-            
-            expect(wrapper.find('.TreeItem__button').exists()).toBe(true);
-        })
-
-        it('should render with id attribute',() => {
-            const props = {
-                id: 'id',
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-
-            expect(wrapper.find('#id__button').exists()).toBe(true);
-        })
-
-        it('should render with href attribute',() => {
-            const props = {
-                href: jest.fn().mockReturnValue('someLink'),
-            }
-
-            const wrapper = getWrapper(defaultProps, props)
-            
-            expect(wrapper.find('.TreeItem__button').first().find('a').prop('href')).toBe('someLink')
-        })
-
-        it('should render with active className',() => {
-            const obj = {foo: 'bar'}
-
-            const props = {
-                selected: obj,
-                item: obj,
-            }
-    
-            const wrapper = getWrapper(defaultProps, props)
-            
-            expect(wrapper.find('.TreeItem__button--active').exists()).toBe(true);
-
-            getKey.mockReturnValue('test')
-
-            const propsTwo = {
-                selected: 'test',
-            }
-
-            const wrapperTwo = getWrapper(defaultProps, propsTwo)
-            
-            expect(wrapperTwo.find('.TreeItem__button--active').exists()).toBe(true);
-        })
-
-        it('should call renderItem function',() => {
-            const mockRenderItem = jest.fn()
-
-            const props = {
-                renderItem: mockRenderItem,
-            }
-    
-            getWrapper(defaultProps, props)
-            
-            expect(mockRenderItem).toHaveBeenCalledTimes(1)
-
-            const mockRenderItemTwo = jest.fn()
-
-            const propsTwo = {
-                renderItem: mockRenderItemTwo,
-            }
-    
-            getWrapper(defaultProps, propsTwo)
-            
-            expect(mockRenderItemTwo).toHaveBeenCalledTimes(1)
-        })
-    })
-
-    describe('renderChildren', () => {
-
-        const defaultProps = {
-            hasChildren: jest.fn(),
-                state: {
-                    open: true,
-                    fetching: true,
-                    children: [{}],
-                },
-            renderItem: jest.fn(),
-            isClickable: true,
-            id: 'testId',
-            loadMore: jest.fn(),
-            labels: {
-                loadMore: 'test_label',
-            },
-            childrenPerPage: 1,
-        }
-
-        it('should render without error',() => {
-            const wrapper = getWrapper(defaultProps, {})
-            
-            expect(wrapper.find('.TreeItem__subtree').exists()).toBe(true);
-        })
-
-        it('should render null',() => {
-            getKey.mockReturnValue(Math.random().toString(36).substr(2, 9))
-
-            expect(getWrapper(defaultProps, {
-                state: {
-                    open: false,
-                },
-            }).find('.TreeItem__subtree').exists()).toBe(false);
-
-            expect(getWrapper(defaultProps, {
-                state: {
-                    children: [],
-                },
-            }).find('.TreeItem__subtree').exists()).toBe(false);
-
-            expect(getWrapper(defaultProps, {
-                state: {
-                    children: null,
-                },
-            }).find('.TreeItem__subtree').exists()).toBe(false);
-        })
-
-        it('should render Button element', () => {
-            const wrapper = getWrapper(defaultProps, {})
-
-            expect(wrapper.find('.TreeItem__button-load-more').exists()).toBe(true);
-            expect(wrapper.find('#testId__load-more').exists()).toBe(true);
-            expect(wrapper.find('.TreeItem__button-load-more').at(0).text()).toBe('test_label');
-        })
-
-        it('should call loadMore function after Button is clicked', () => {
-            const mockLoadMore = jest.fn()
-
-            const props = {
-                loadMore: mockLoadMore,
-            }
-    
-            const button = getWrapper(defaultProps, props).find('.TreeItem__button-load-more').at(0)
-
-            button.simulate('click')
-            
-            expect(mockLoadMore).toHaveBeenCalledTimes(1)
-        })
-    })
-})
+      expect(mockLoadMore).toHaveBeenCalledTimes(1);
+    });
+  });
+});
