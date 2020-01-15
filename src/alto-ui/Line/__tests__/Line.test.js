f08a228b6367444c17ac8d543656a4282def5c04
krolwofficial <krolwofficial@gmail.com>
2020-01-15 11:25:22 +0100

    Lint and prettier code

--- src/alto-ui/Line/__tests__/Line.test.js
+++ src/alto-ui/Line/__tests__/Line.test.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 
-import Line from './../Line'
+import Line from './../Line';
 
 describe('Line', () => {
-    
-    const getWrapper = props => shallow(<Line {...props} />);
-    
-    it('should render without error',() => {
-        const wrapper = getWrapper({});
+  const getWrapper = props => shallow(<Line {...props} />);
 
-        expect(wrapper.find('.Line').exists()).toBe(true);
-    });
+  it('should render without error', () => {
+    const wrapper = getWrapper({});
 
-    it('should return element with className from props', () => {
-        const props = {
-            vertical: true,
-        }
+    expect(wrapper.find('.Line').exists()).toBe(true);
+  });
 
-        const wrapper = getWrapper(props);
+  it('should return element with className from props', () => {
+    const props = {
+      vertical: true,
+    };
 
-        expect(wrapper.find('.Line--vertical').exists()).toBe(true);
-    });
+    const wrapper = getWrapper(props);
 
-    it('should pass custom props to Line element', () => {
-        expect(getWrapper({propsMock: true}).find('.Line').prop('propsMock')).toBe(true);
-    })
-})
+    expect(wrapper.find('.Line--vertical').exists()).toBe(true);
+  });
+
+  it('should pass custom props to Line element', () => {
+    expect(
+      getWrapper({ propsMock: true })
+        .find('.Line')
+        .prop('propsMock')
+    ).toBe(true);
+  });
+});
