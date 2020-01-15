a491ddd82759994be277fbc6b4b73524a2b89e7a
krolwofficial <krolwofficial@gmail.com>
2020-01-15 11:56:21 +0100

    Lint and prettier code

--- src/alto-ui/Spinner/__tests__/Spinner.test.js
+++ src/alto-ui/Spinner/__tests__/Spinner.test.js
@@ -3,67 +3,66 @@ import { shallow } from 'enzyme';
 
 import Spinner from '../Spinner';
 
-jest.mock('../../CloseButton', () => ({onClick}) => (
+jest.mock('../../CloseButton', () => ({ onClick }) => (
   <button className="CloseButton" onClick={() => onClick()} />
 ));
 
 describe('Spinner', () => {
-  
-    const getWrapper = props => shallow(<Spinner {...props} />)
+  const getWrapper = props => shallow(<Spinner {...props} />);
 
-    it('should return null if show props are falsy',() => {
-        const props = {
-            show: false,
-        }
+  it('should return null if show props are falsy', () => {
+    const props = {
+      show: false,
+    };
 
-        const wrapper = getWrapper(props)
+    const wrapper = getWrapper(props);
 
-        expect(wrapper.getElement()).toBeNull()
-    })
+    expect(wrapper.getElement()).toBeNull();
+  });
 
-    it('should return children if show props are falsy',() => {
-        const props = {
-            show: false,
-            children: 'true',
-        }
+  it('should return children if show props are falsy', () => {
+    const props = {
+      show: false,
+      children: 'true',
+    };
 
-        const wrapper = getWrapper(props)
+    const wrapper = getWrapper(props);
 
-        expect(wrapper.text()).toBe('true')
-    })
+    expect(wrapper.text()).toBe('true');
+  });
 
-    it('should render Spinner element with props',() => {
-        const props = {
-            id: 'id',
-            small: true,
-            large: true,
-        }
+  it('should render Spinner element with props', () => {
+    const props = {
+      id: 'id',
+      small: true,
+      large: true,
+    };
 
-        const wrapper = getWrapper(props)
+    const wrapper = getWrapper(props);
 
-        expect(wrapper.find('#id').exists()).toBe(true);
-        expect(wrapper.find('.Spinner--small').exists()).toBe(true);
-        expect(wrapper.find('.Spinner--large').exists()).toBe(true);
-    })
+    expect(wrapper.find('#id').exists()).toBe(true);
+    expect(wrapper.find('.Spinner--small').exists()).toBe(true);
+    expect(wrapper.find('.Spinner--large').exists()).toBe(true);
+  });
 
-    it('should render element with Spinner--centered__wrapper className',() => {
-        const props = {
-            centered: true,
-        }
+  it('should render element with Spinner--centered__wrapper className', () => {
+    const props = {
+      centered: true,
+    };
 
-        const wrapper = getWrapper(props)
+    const wrapper = getWrapper(props);
 
-        expect(wrapper.find('.Spinner--centered__wrapper').exists()).toBe(true);
-    })
+    expect(wrapper.find('.Spinner--centered__wrapper').exists()).toBe(true);
+  });
 
-    it('should render spinner inside of Spinner--centered__wrapper element',() => {
-        const props = {
-            centered: true,
-        }
+  it('should render spinner inside of Spinner--centered__wrapper element', () => {
+    const props = {
+      centered: true,
+    };
 
-        const wrapper = getWrapper(props)
+    const wrapper = getWrapper(props);
 
-        expect(wrapper.find('.Spinner--centered__wrapper').exists()).toBe(true);
-        expect(wrapper.find('.Spinner').exists()).toBe(true);
-    })
-})
+    expect(wrapper.find('.Spinner--centered__wrapper').exists()).toBe(true);
+    expect(wrapper.find('.Spinner').exists()).toBe(true);
+  });
+});
